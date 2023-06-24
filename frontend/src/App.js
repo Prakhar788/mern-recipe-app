a14bb6fee63351439f8482a52730f5495d30f83d
Prakhar Sharma <prakhar.sharma_cs20@gla.ac.in>
2023-06-24 21:08:20 +0530

    Adding new Files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import "../src/App.css"
+import {Routes,Route} from "react-router-dom";
+import Home from "./pages/home"
+import {Auth} from "./pages/auth"
+import CreateRecipe from "./pages/create-recipe"
+import SavedRecipes from "./pages/saved-recipes"
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/auth' element={<Auth/>} />
+      <Route path='/create-recipe' element={<CreateRecipe/>} />
+      <Route path='/saved-recipes' element={<SavedRecipes/>} />
+    </Routes>
+    </div>
   )
 }
 
